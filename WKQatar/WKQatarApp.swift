368a82f3df8278adc9251bb4ef20b0789d93bfb1
Aden Vansevenant <adenvansevenant@iMacP.local>
2025-10-21 11:32:09 +0200

    first commit

---
+++ WKQatar/WKQatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKQatarApp.swift
+//  WKQatar
+//
+//  Created by Aden Vansevenant       on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct WKQatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
